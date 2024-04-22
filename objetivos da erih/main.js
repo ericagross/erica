abc8b5700d1fef8c196593777df0427ee0b24c4d
Érica Gross <128057913+ericagross@users.noreply.github.com>
2024-04-22 12:04:00 -0300

    Add files via upload

--- objetivos da erih/main.js
+++ objetivos da erih/main.js
@@ -1,14 +1,22 @@
 const botoes = document.querySelectorAll(".botao");
+const textos= document.querySelectorAll(".aba-conteudo");
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
+
+
 
 for(let i=0;i <botoes.length;i++){
     //Variável "i" é igual a 0, mas enquanto essa variável for menor que a quantidade de elementos nessa lista, aumentamos o valor de "i", ou seja, i será a quantidade de botões.
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) {
         botoes[j].classList.remove("ativo");
+       textos[j].classList.remove("ativo");
+
+
         }
         //Remove a classe "ativo" de todos os botões.
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+
         //Adiciona a classe ativo só no botão clicado.
     }
 }
