8f200548fdd914a2b9a0a09ae83c25e6e310bc9f
Érica Gross <128057913+ericagross@users.noreply.github.com>
2024-04-29 11:45:14 -0300

    Add files via upload

--- erica-main/erica-main/objetivos da erih/main.js
+++ erica-main/erica-main/objetivos da erih/main.js
@@ -1,27 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
-const textos= document.querySelectorAll(".aba-conteudo");
-const contadores=document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date("2034-05-05T00:00:00");
-const tempoatual= new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
-
 
 for(let i=0;i <botoes.length;i++){
     //Variável "i" é igual a 0, mas enquanto essa variável for menor que a quantidade de elementos nessa lista, aumentamos o valor de "i", ou seja, i será a quantidade de botões.
     botoes[i].onclick = function() {
         for(let j=0;j<botoes.length;j++) {
         botoes[j].classList.remove("ativo");
-       textos[j].classList.remove("ativo");
-
-
         }
         //Remove a classe "ativo" de todos os botões.
         botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
-
         //Adiciona a classe ativo só no botão clicado.
     }
 }
-
-
-contadores[0].textContent=tempoObjetivo1-tempoatual;
